f406a3d97b28169910e61f6f7859872ea0be5ad7
juliilp <105088200+juliilp@users.noreply.github.com>
2023-03-19 02:29:32 -0300

    email deploy

--- src/email/email.js
+++ src/email/email.js
@@ -3,12 +3,15 @@ import axios from "axios";
 const emailPost = async (nombre, correo, mensaje, apellido) => {
   console.log(nombre, correo, mensaje, apellido);
   try {
-    const emailapi = await axios.post("http://localhost:3001/enviar", {
-      nombre,
-      correo,
-      mensaje,
-      apellido,
-    });
+    const emailapi = await axios.post(
+      "https://portfolio-back-delta.vercel.app/enviar",
+      {
+        nombre,
+        correo,
+        mensaje,
+        apellido,
+      }
+    );
     return emailapi;
   } catch (error) {
     console.log(error.message);
